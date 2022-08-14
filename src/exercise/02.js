3e3ade29e4fb9cd9e89f9c5e5808827a2ffe3e36
jbontk <jbontik@gmail.com>
2022-08-14 16:56:17 +0000

    Exercise 2 - my answer

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,40 +4,33 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
+function Toggle({children}) {
+    const [on, setOn] = React.useState(false)
+    const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+    return React.Children.map(children, (child, index) => {
+        //console.log(child)
+        return child.type.name === 'ToggleButton'
+            ? React.cloneElement(child, {on, toggle})
+            : React.cloneElement(child, {on}) // using React.cloneElement to pass down the props that we want to the child components
+            // , without needing to pass the props from the parent component
+    })
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleOn = ({on, children}) => on ? <>{children}</> : null;
+const ToggleOff = ({on, children}) => on ? null : <>{children}</>
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle}/>
 
 function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
-      </Toggle>
-    </div>
-  )
+    return (
+        <div>
+            <Toggle>
+                <ToggleOn>The button is on</ToggleOn>
+                <ToggleOff>The button is off</ToggleOff>
+                <ToggleButton/>
+            </Toggle>
+        </div>
+    )
 }
 
 export default App
