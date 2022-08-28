138fabb22706fe6102a7ef740042e6dc60012dc7
jbontk <jbontik@gmail.com>
2022-08-28 15:43:14 +0000

    Exercise 5: override the default reducer (toggleReducer) by toggleStateReducer, which locks the toggle on "on" when the user clicked too much. For that, we had to allow useToggle to accept a reducer function as input argument

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -21,12 +21,9 @@ function toggleReducer(state, {type, initialState}) {
 }
 
 // 🐨 add a new option called `reducer` that defaults to `toggleReducer`
-function useToggle({initialOn = false} = {}) {
+function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const {current: initialState} = React.useRef({on: initialOn})
-  // 🐨 instead of passing `toggleReducer` here, pass the `reducer` that's
-  // provided as an option
-  // ... and that's it! Don't forget to check the 💯 extra credit!
-  const [state, dispatch] = React.useReducer(toggleReducer, initialState)
+  const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
   const toggle = () => dispatch({type: 'toggle'})
@@ -85,7 +82,6 @@ function App() {
     <div>
       <Switch
         {...getTogglerProps({
-          disabled: clickedTooMuch,
           on: on,
           onClick: () => setTimesClicked(count => count + 1),
         })}
